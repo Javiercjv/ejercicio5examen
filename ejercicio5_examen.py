9561bbe47ee09432bafcd9b6ba10caf53065ef71
Javiercjv <javiercastellvelasco@gmail.com>
2023-05-30 11:31:26 +0200

    initial commit

---
+++ ejercicio5_examen.py
@@ -0,0 +1,19 @@
+alumnos = {
+    "alumno 1" : {
+        "nombre" : "Juan Perez",
+        "edad" : 18,
+        "curso" : 12,
+    },
+
+    "alumno 2": {
+        "nombre" : "Maria Lopez",
+        "edad" : 17,
+        "curso" : 11,
+    },
+    
+    "alumno 3" : {
+        "nombre" : "Pedro Gomez",
+        "edad" : 19,
+        "curso" : 12
+    }
+}
