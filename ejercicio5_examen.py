31a3b1d1fc0251322acb2fdb102324163d5d6c5e
Javiercjv <javiercastellvelasco@gmail.com>
2023-05-30 11:54:39 +0200

    fix

--- ejercicio5_examen.py
+++ ejercicio5_examen.py
@@ -1,6 +1,5 @@
 alumnos = [
-    ["nombre":"Juan Perez", "edad": 18, "curso": 12]
-    ["nombre":"Maria Lopez", "edad": 17, "curso": 11]
-    ["nombre" :"Juan Perez", "edad": 19, "curso": 12]
-
+    {"id":1, "nombre":"Juan Perez", "edad": 18, "curso": 12},
+    {"id":2, "nombre":"Maria Lopez", "edad": 17, "curso": 11},
+    {"id": 3, "nombre" :"Juan Perez", "edad": 19, "curso": 12},
 ]
